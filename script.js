2259f795148350941f463c552cec4f553f749dd3
Lugabe <luis2014gbp@gmail.com>
2023-06-14 22:56:07 -0300

    Adicionado a conversão para a moeda Euro

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const convertBotton = document.querySelector(".convert-button")
+const convertBotton = document.querySelector(".convert-button");
 
 function botaoApertado() {
-
+    const seletorMoedas = document.querySelector(".seletor");
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
     const currencyValueConverted = document.querySelector(".currency-value");
     const inputCurrencyValue = document.querySelector(".input").value;
-    const dolarToday = 5.20;
 
-    const convertedValue = (inputCurrencyValue / dolarToday);
+    const dolarToday = 5.20;
+    const euroToday = 6.20;
+
+    const convertedValueEuro = (inputCurrencyValue / euroToday);
+    const convertedValueDolar = (inputCurrencyValue / dolarToday);
+
+    if (seletorMoedas.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency", //moeda
+            currency: "USD"
+        }).format(convertedValueDolar);
+
+    }
+    if (seletorMoedas.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-PT", {
+            style: "currency", //moeda
+            currency: "EUR"
+        }).format(convertedValueEuro);
+    }
+
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency", //moeda
@@ -15,9 +33,8 @@ function botaoApertado() {
     }).format(inputCurrencyValue);
 
 
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency", //moeda
-        currency: "USD"
-    }).format(convertedValue);
+
+
+
 
 }
